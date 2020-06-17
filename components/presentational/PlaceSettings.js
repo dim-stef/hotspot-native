7b79ec8b448c1851665544b0a57478d2102a4c24
Ilias Chymas <iliaschymas@outlook.com>
2020-06-17 15:14:38 +0300

    Added google auto complete

--- components/presentational/PlaceSettings.js
+++ components/presentational/PlaceSettings.js
@@ -1,8 +1,27 @@
 import React from 'react';
-import {View} from 'react-native';
+import Config from 'react-native-config';
+import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
 
 function PlaceSettings() {
-  return <View />;
+  return (
+    <>
+      <GooglePlacesAutocomplete
+        placeholder="Search"
+        onPress={(data, details = null) => {
+          // 'details' is provided when fetchDetails = true
+          console.log(data);
+        }}
+        onChangeText={data => {
+          console.log(data);
+        }}
+        onFail={error => console.log(error)}
+        query={{
+          key: Config.GOOGLE_API_KEY,
+          language: 'en',
+        }}
+      />
+    </>
+  );
 }
 
 export default PlaceSettings;
