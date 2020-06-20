289f3436f5ea798f16ab7629ee86d889ecfeafc8
dim <jimstef@outlook.com>
2020-06-20 04:00:18 +0300

    added some user settings

--- components/presentational/PlaceSettings.js
+++ components/presentational/PlaceSettings.js
@@ -1,12 +1,76 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, {useState, useEffect} from 'react';
 import Config from 'react-native-config';
 import {Text} from 'react-native';
 import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
 import GetLocation from 'react-native-get-location';
+import AsyncStorage from '@react-native-community/async-storage';
 import MapView from 'react-native-maps';
 
 function PlaceSettings() {
   const [geolocation, setGeolocation] = useState();
+  const [place, setPlace] = useState(null);
+
+  async function getPlaceDetails() {
+    try {
+      let response = await fetch(
+        `https://maps.googleapis.com/maps/api/place/details/json?language=el&placeid=${
+          place.place_id
+        }&key=${Config.GOOGLE_API_KEY}`,
+      );
+      let json = await response.json();
+      console.log(json);
+      createLocation(json.result);
+      //return json.movies;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function createLocation(data) {
+    let addressLocality = data.address_components.find(component =>
+      component.types.includes('locality'),
+    );
+    console.log('data', data, addressLocality);
+    let token = null;
+    try {
+      token = await AsyncStorage.getItem('token');
+    } catch (e) {
+      // saving error
+    }
+    if (token && addressLocality) {
+      try {
+        let response = await fetch(`${Config.API_URL}/locations/`, {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify({
+            place_id: data.place_id,
+            long_name: addressLocality.long_name,
+            short_name: addressLocality.short_name,
+            latitude: data.geometry.location.lat,
+            longitude: data.geometry.location.lng,
+          }),
+        });
+        let json = await response.json();
+        console.log(json);
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      console.log('iner', token);
+    }
+  }
+
+  useEffect(() => {
+    if (place) {
+      getPlaceDetails();
+    }
+  }, [getPlaceDetails, place]);
+
   useEffect(() => {
     getLocation();
   }, []);
@@ -19,6 +83,7 @@ function PlaceSettings() {
           onPress={(data, details = null) => {
             // 'details' is provided when fetchDetails = true
             console.log(data);
+            setPlace(data);
           }}
           onChangeText={data => {
             console.log(data);
@@ -26,7 +91,8 @@ function PlaceSettings() {
           onFail={error => console.log(error)}
           query={{
             key: Config.GOOGLE_API_KEY,
-            language: 'en',
+            language: 'el',
+            components: 'country:gr',
           }}
         />
         <MapView
